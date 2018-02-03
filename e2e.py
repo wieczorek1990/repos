0dd41d7a58c63127e34e1f1bf66cac32705aa864
Łukasz Wieczorek <wieczorek1990@gmail.com>
2018-02-03 10:56:47 +0100

    Add E2E test

--- e2e.py
+++ e2e.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 import requests
 from rest_framework import status
 
@@ -7,6 +8,12 @@ class RepositoriesTestCase(unittest.TestCase):
     """Tests the actual living localhost server instance."""
 
     HOST = 'http://localhost:8000'
+
+    def assertIsUrl(self, string):
+        return string.startswith('https://')
+
+    def assertIsISO8601(self, string):
+        return re.match(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z', string)
 
     def test_wieczorek1990(self):
         response = requests.get(
@@ -19,8 +26,26 @@ class RepositoriesTestCase(unittest.TestCase):
         self.assertIsInstance(data['fullName'], str)
         self.assertIsNone(data['description'])
         self.assertIsInstance(data['cloneUrl'], str)
+        self.assertIsUrl(data['cloneUrl'])
         self.assertIsInstance(data['stars'], int)
         self.assertIsInstance(data['createdAt'], str)
+        self.assertIsISO8601(data['createdAt'])
+
+    def test_torvalds(self):
+        response = requests.get(
+            '{host}/repositories/torvalds/linux/'.format(
+                host=self.HOST,
+            )
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+        self.assertIsInstance(data['fullName'], str)
+        self.assertIsInstance(data['description'], str)
+        self.assertIsInstance(data['cloneUrl'], str)
+        self.assertIsUrl(data['cloneUrl'])
+        self.assertIsInstance(data['stars'], int)
+        self.assertIsInstance(data['createdAt'], str)
+        self.assertIsISO8601(data['createdAt'])
 
 
 if __name__ == '__main__':
