20dca8fa53e17de1a91ac656b1ae420e2689c1d3
Łukasz Wieczorek <wieczorek1990@gmail.com>
2018-02-13 17:54:22 +0100

    Move github_api variable initialization to globals

--- repositories/api/views.py
+++ repositories/api/views.py
@@ -8,14 +8,15 @@ from api import models
 from api import serializers
 
 
+github_api = github.Github(settings.GITHUB_ACCESS_TOKEN)
+
+
 class RepositoriesView(views.APIView):
     """View serving repository details."""
 
-    @staticmethod
-    def get_external_data(owner, repository_name):
+    def get_external_data(self, owner, repository_name):
         """Returns data for the serializer."""
 
-        github_api = github.Github(settings.GITHUB_ACCESS_TOKEN)
         repository = github_api.get_repo(
             '{}/{}'.format(owner, repository_name), lazy=False
         )
